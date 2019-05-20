2ca122e197e6fb0faa5dbc92ec3525ea97ad379b
github username <github email address>
2019-05-20 17:19:25 -0400

    The return value of .each is the array that is iterated upon

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  sqarray = []
+  array.each do |square_element|
+    square_element = square_element ** 2
+    sqarray.push(square_element)
+  end
+  return sqarray
 end
